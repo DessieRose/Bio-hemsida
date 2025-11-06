7d4d180ae4770e4f05b07e126caafa746af98a89
Malin Persson <malin.persson02@hotmail.com>
2025-11-06 14:37:05 +0100

    Added data.php & movie page.php. Added hero image and a function to loop movies

---
+++ data.php
@@ -0,0 +1,16 @@
+<?php
+
+$movies = [
+    [
+        "id" => 1,
+        "title" => "Weapons",
+        "year" => 2025,
+        "rated" => "R",
+        "director" => "Zach Cregger",
+        "genre" => "Dark comedy, Psychological Horror",
+        "poster" => "/components/movies/weapons.png",
+        "synopsis" => "When all but one child from the same class mysteriously vanish on the same night at exactly the same time, a community is left questioning who or what is behind their disappearance.",
+        "trailer" => "",
+        "media" => []
+    ],
+];
--- index.php
+++ index.php
@@ -1,16 +1,32 @@
-<?php 
-    require_once __DIR__ . "/header.php";     
+<?php
+require_once __DIR__ . "/header.php";
+require_once __DIR__ . "/data.php";
 ?>
 
-    <main>
+<main>
+    <div class="hero-container">
+        <img class="hero-image" src="/components/heroimage.png" alt="Horror themed hero image.">
+    </div>
+    <section class="movies">
+        <h3>Movies</h3>
+        <?php foreach ($movies as $movie) { ?>
+            <div class="movie-container">
+                <img class="movieroll" src="/components/filmrulle snutt.svg">
+                <img class="poster" src="<?= $movie['poster'] ?>">
+                <p class="title"><?= $movie['title'] ?></p>
+            <?php } ?>
+            </div>
+    </section>
+    <section class="upcoming">
+        <h3>Upcoming</h3>
+    </section>
 
-        
 
-    </main>
-    
-    
+</main>
+
+
 </body>
 
-<?php 
-    require_once __DIR__ . "/footer.php";     
+<?php
+require_once __DIR__ . "/footer.php";
 ?>
