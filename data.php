1ef58966bd21bb0b991a64fda3a3ba4a9645858c
Malin Persson <malin.persson02@hotmail.com>
2025-11-07 13:19:26 +0100

    Fixed a grid layout to movies and upcoming

--- data.php
+++ data.php
@@ -13,4 +13,40 @@ $movies = [
         "trailer" => "",
         "media" => []
     ],
+    [
+        "id" => 1,
+        "title" => "Black Phone 2",
+        "year" => 2025,
+        "rated" => "R",
+        "director" => "Zach Cregger",
+        "genre" => "Dark comedy, Psychological Horror",
+        "poster" => "/components/movies/black phone 2.png",
+        "synopsis" => "When all but one child from the same class mysteriously vanish on the same night at exactly the same time, a community is left questioning who or what is behind their disappearance.",
+        "trailer" => "",
+        "media" => []
+    ],
+    [
+        "id" => 1,
+        "title" => "Good Boy",
+        "year" => 2025,
+        "rated" => "R",
+        "director" => "Zach Cregger",
+        "genre" => "Dark comedy, Psychological Horror",
+        "poster" => "/components/movies/good boy.png",
+        "synopsis" => "When all but one child from the same class mysteriously vanish on the same night at exactly the same time, a community is left questioning who or what is behind their disappearance.",
+        "trailer" => "",
+        "media" => []
+    ],
+    [
+        "id" => 1,
+        "title" => "The Sinners",
+        "year" => 2025,
+        "rated" => "R",
+        "director" => "Zach Cregger",
+        "genre" => "Dark comedy, Psychological Horror",
+        "poster" => "/components/movies/sinners.png",
+        "synopsis" => "When all but one child from the same class mysteriously vanish on the same night at exactly the same time, a community is left questioning who or what is behind their disappearance.",
+        "trailer" => "",
+        "media" => []
+    ],
 ];
--- index.php
+++ index.php
@@ -9,16 +9,27 @@ require_once __DIR__ . "/data.php";
     </div>
     <section class="movies">
         <h3>Movies</h3>
-        <?php foreach ($movies as $movie) { ?>
-            <div class="movie-container">
-                <img class="movieroll" src="/components/filmrulle snutt.svg">
-                <img class="poster" src="<?= $movie['poster'] ?>">
-                <p class="title"><?= $movie['title'] ?></p>
+        <div class="movies-container">
+            <?php foreach ($movies as $movie) { ?>
+                <div class="movie-container">
+                    <img class="movieroll" src="/components/filmrulle snutt.svg">
+                    <img class="poster" src="<?= $movie['poster'] ?>">
+                    <p class="title"><?= $movie['title'] ?></p>
+                </div>
             <?php } ?>
-            </div>
+        </div>
     </section>
     <section class="upcoming">
         <h3>Upcoming</h3>
+        <div class="movies-container">
+            <?php foreach ($movies as $movie) { ?>
+                <div class="movie-container">
+                    <img class="movieroll" src="/components/filmrulle snutt.svg">
+                    <img class="poster" src="<?= $movie['poster'] ?>">
+                    <p class="title"><?= $movie['title'] ?></p>
+                </div>
+            <?php } ?>
+        </div>
     </section>
 
 
