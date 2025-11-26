8bb6e6a94b5b24bfbe83e4aa04f0ad4cd5fd2168
Nathalie Rosenkvist <rosenkvist98@gmail.com>
2025-11-26 10:27:50 +0100

    Updated: movie data and adjust header styles

--- data.php
+++ data.php
@@ -9,9 +9,9 @@ $movies = [
         "director" => "Zach Cregger",
         "genre" => "Dark comedy, Psychological Horror",
         "poster" => "/components/movies/weapons.png",
-        "hover-info" => "Hovering here for some info",
+        "hover-info" => "a horror-mystery about a small town where 17 children vanish overnight at 2:17 a.m",
         "synopsis" => "When all but one child from the same class mysteriously vanish on the same night at exactly the same time, a community is left questioning who or what is behind their disappearance.",
-        "trailer" => "",
+        "trailer" => "https://youtu.be/OpThntO9ixc?si=626erNjl80W49B6T",
         "media" => []
     ],
     [
@@ -19,7 +19,7 @@ $movies = [
         "title" => "Trick r' Treat",
         "year" => 2007,
         "rated" => "R",
-        "director" => "Zach Cregger",
+        "director" => "Michael Dougherty",
         "genre" => "Dark comedy, Monster horror",
         "poster" => "/components/movies/trick r treat.jpg",
         "hover-info" => "Hovering here for some info",
