a15fad193ffd0daf603a18c633e7c7df9fd4372e
Nathalie Rosenkvist <rosenkvist98@gmail.com>
2025-11-28 16:34:53 +0100

    Updated: data.php

--- data.php
+++ data.php
@@ -112,3 +112,102 @@ $movies = [
         ]
     ],
 ];
+
+
+$retroruns = [
+    [
+        "id" => 1,
+        "title" => "Weapons",
+        "year" => 2025,
+        "rated" => "R",
+        "director" => "Zach Cregger",
+        "genres" => ["Dark comedy", "Psychological Horror"],
+        "poster" => "./components/movies/weapons.png",
+        "hover-info" => "A horror-mystery about a small town where 17 children vanish overnight at 2:17 a.m",
+        "synopsis" => [],
+        "trailer" => "https://www.youtube.com/watch?v=OpThntO9ixc",
+        "actors" => ["Scarlett Sher", "Julia Garner", "Cary Christopher"],
+        "media" => [
+            "./media/wepons/1.jpeg",
+            "./media/wepons/2.jpeg",
+            "./media/wepons/3.jpeg",
+            "./media/wepons/4.jpg",
+        ]
+    ],
+    [
+        "id" => 2,
+        "title" => "Trick r' Treat",
+        "year" => 2007,
+        "rated" => "11",
+        "director" => "Michael Dougherty",
+        "genres" => ["Dark comedy", "Slasher horror", "Supernatural horror", "Monster horror", "Comedy", "Horror"],
+        "poster" => "./components/movies/trick-r-treat.jpg",
+        "hover-info" => "Interconnected Halloween horror tales with deadly twists.",
+        "synopsis" => [],
+        "trailer" => "https://www.youtube.com/embed/AoBx7F-q2CA?autoplay=1&mute=1&controls=0",
+        "actors" => ["Dylan Baker", "Rochelle Aytes", "Quinn Lord"],
+        "media" => [
+            "./media/trick/1.png",
+            "./media/trick/2.png",
+            "./media/trick/3.png",
+            "./media/trick/4.png",
+        ]
+    ],
+    [
+        "id" => 3,
+        "title" => "Good Boy",
+        "year" => 2025,
+        "rated" => "15",
+        "director" => "Ben Leonberg",
+        "genres" => ["Supernatural Horror", "Thriller"],
+        "poster" => "./components/movies/good-boy.png",
+        "hover-info" => "A haunted-house told entirely from the POV of a loyal dog, who senses supernatural forces.",
+        "synopsis" => [],
+        "trailer" => "https://www.youtube.com/watch?v=VFqG99KuPV8",
+        "actors" => ["Indy", "Shane Jensen", "Arielle Friedman"],
+        "media" => [
+            "./media/good-boy/1.jpg",
+            "./media/good-boy/2.jpg",
+            "./media/good-boy/3.jpg",
+            "./media/good-boy/4.jpg",
+        ]
+    ],
+    [
+        "id" => 4,
+        "title" => "Sinners",
+        "year" => 2025,
+        "rated" => "15",
+        "director" => "Ryan Coogler",
+        "genres" => ["Folk Horror", "Period Drama", "Supernatural Horror"],
+        "poster" => "./components/movies/sinners.png",
+        "hover-info" => "Twin brothers return home in 1930s Mississippi, only to awaken an ancient evil.",
+        "synopsis" => [],
+        "trailer" => "https://www.youtube.com/watch?v=bKGxHflevuk",
+        "actors" => ["Miles Caton", "Saul Williams", "Andrene Ward-Hammond"],
+        "media" => [
+            "./media/sinners/1.jpg",
+            "./media/sinners/2.jpg",
+            "./media/sinners/3.jpg",
+            "./media/sinners/4.jpg",
+        ]
+    ],
+    [
+        "id" => 5,
+        "title" => "Scream VI",
+        "year" => 2023,
+        "rated" => "15",
+        "director" => ["Matt Bettinelli-Olpin", "Tyler Gillett"],
+        "genres" => ["Dark comedy", "Slasher Horror", "Suspense Mystery"],
+        "poster" => "./components/movies/scream-iv.jpg",
+        "hover-info" => "The survivors face a deadlier Ghostface in New York City.",
+        "synopsis" => [],
+        "trailer" => "https://www.youtube.com/watch?v=h74AXqw4Opc",
+        "actors" => ["Courteney Cox", "Melissa Barrera", "Jenna Ortega"],
+        "media" => [
+            "./media/scream/1.jpg",
+            "./media/scream/2.jpg",
+            "./media/scream/3.jpg",
+            "./media/scream/4.jpg",
+        ]
+    ],
+];
