3a73e82c4f994ce48dec2da39f7b0c0cde431449
EmmaRask <emmarask@yahoo.com>
2025-11-28 08:54:07 +0100

    uppdated css and html in footer

--- footer.php
+++ footer.php
@@ -1,7 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Brawler:wght@400;700&family=Creepster&family=Road+Rage&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="footer.css">
+    <title>The Big Scream</title>
+</head>
+
 
 <script src="script.js"></script>
-</body>
+<body>
+
+<section class="halloween-offer">
+    <div class="offer-header">
+        <div class="offer-sticker">
+            <img src="components/icons/offer_sticker_computer.png" alt="Halloween Offer">
+        </div>
+        <div class="offer-title">
+            <h3>HALLOWEEN OFFER!</h3>
+        </div>
+    </div>
+
+    <div class="offer-text">
+        <p>
+            Get 30% off all tickets this halloween!<br>
+            Valid for all new tickets purchased between October 20th and November 10th.<br>
+            Use offercode: SPOOKY30 at checkout
+        </p>
+    </div>
+</section>
+
+
+
+<div class="newsletter">
+    <h4>
+        Never miss anything new!<br>
+        Sign up for our newsletter
+    </h4>
+
+<form>
+    <div class="form-row">
+        <label for="Fullname">Fullname</label>
+        <input type="text" name="Fullname">
+    </div>
+
+    <div class="form-row">
+        <label for="Email">Email</label>
+        <input type="text" name="Email">
+    </div>
+
+    <button type="submit">Submit</button>
+</form>
+
+</div>
+
+    
 <footer>
+    <div class="boxfooter">
+        <div class= "box1">
+            <h3>Our company</h3>
+            <ul>
+                <li>Locations</li>
+                <li>About</li>
+                <li>Contact us</li> 
+            </ul>
+        </div>
+
+        <div class= "box3">
+            <h3>More</h3>
+            <ul>
+                <li><a href="Tickets.html">Buy tickets</a></li>
+                <li>Buy giftcard</li>
+                <li>Deals</li>
+                <li>Experiences</li>
+                <li>Service</li>
+            </ul>
+        </div>
+
+        <div class= "box4">
+            <h3>Socials</h3>
+            <ul>
+                <li>
+                    <img src="components/icons/facebook.jpg" alt="The Big Scream">
+                    <span>The Big Scream</span>
+                </li>
+                <li>
+                    <img src="components/icons/mail.jpg" alt="@thebigscream">
+                    <span>@thebigscream</span>
+                </li>   
+                <li>
+                    <img src="components/icons/instagram.jpg" alt="The Big Scream">
+                    <span>The Big Scream</span>
+                </li>
+            </ul>
+        </div>
+        <div class= "box2">
+            <img src="components/logo.png" alt="The-big-scream logo">
+                
+            <div class="logo-line"></div>
+
+            <div class="footer-info">
+                <p class="copyright">© 2025 The Big Scream</p>
+                <p class="terms">Terms & Conditions</p>
+                <p class="privacy">Privacy Policy</p>
+            </div>
+            
+        </div>
+    
+    </div>
+
+</footer>
+</body>
+</html>
+
     <!-- <ul>
         <li><a href="index.html">Home</a></li>
         <li><a href="">Movies</a></li>
@@ -10,5 +124,3 @@
         <li><a href="">Get tickets</a></li>
     </ul> -->
 
-</footer>
-</html>
