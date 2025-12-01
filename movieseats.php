68fd87ef6b1a705fb107024dcf0a858478a35f04
EmmaRask <emmarask@yahoo.com>
2025-12-01 12:03:55 +0100

    uppdated movieseatsphp, css js

---
+++ movieseats.php
@@ -0,0 +1,127 @@
+<?php
+require_once __DIR__ . "/header.php";
+require_once __DIR__ . "/data.php";
+?>
+
+    <link rel="stylesheet" href="./movieseats.css">
+
+<body>
+    
+     <section class="booking">
+
+        <ul class="seats">
+
+            <li>
+                <div class="seat"></div>
+                <small>Available</small>
+            </li>
+        
+            <li>
+                <div class="seat selectedseat"></div>
+                <small>Selected</small>
+            </li>
+        
+            <li>
+                <div class="seat occupiedseat"></div>
+                <small>Occupied</small>
+            </li>
+        </ul>
+
+        <div class="theatre">
+
+            <div class="screen"></div>
+
+            <div class="row">
+                <div class="seat"></div>
+                <div class="seat"></div>
+                <div class="seat"></div>
+                <div class="seat occupiedseat"></div>
+                <div class="seat occupiedseat"></div>
+                <div class="seat"></div>
+                <div class="seat"></div>
+                <div class="seat"></div>
+                <div class="seat occupiedseat"></div>
+                <div class="seat"></div>
+            </div>
+
+    
+            <div class="row">
+                <div class="seat"></div>
+                <div class="seat"></div>
+                <div class="seat"></div>
+                <div class="seat"></div>
+                <div class="seat occupiedseat"></div>
+                <div class="seat occupiedseat"></div>
+                <div class="seat occupiedseat"></div>
+                <div class="seat"></div>
+                <div class="seat"></div>
+                <div class="seat"></div>
+            </div>
+
+        
+            <div class="row">
+                <div class="seat"></div>
+                <div class="seat occupiedseat"></div>
+                <div class="seat"></div>
+                <div class="seat"></div>
+                <div class="seat"></div>
+                <div class="seat occupiedseat"></div>
+                <div class="seat"></div>
+                <div class="seat"></div>
+                <div class="seat"></div>
+                <div class="seat"></div>
+            </div>
+
+        
+            <div class="row">
+                <div class="seat"></div>
+                <div class="seat"></div>
+                <div class="seat"></div>
+                <div class="seat"></div>
+                <div class="seat occupiedseat"></div>
+                <div class="seat"></div>
+                <div class="seat occupiedseat"></div>
+                <div class="seat occupiedseat"></div>
+                <div class="seat"></div>
+                <div class="seat"></div>
+            </div>
+
+        
+            <div class="row">
+                <div class="seat"></div>
+                <div class="seat occupiedseat"></div>
+                <div class="seat occupiedseat"></div>
+                <div class="seat"></div>
+                <div class="seat"></div>
+                <div class="seat occupiedseat"></div>
+                <div class="seat occupiedseat"></div>
+                <div class="seat"></div>
+                <div class="seat"></div>
+                <div class="seat"></div>
+            </div>
+
+        
+            <div class="row">
+                <div class="seat"></div>
+                <div class="seat"></div>
+                <div class="seat"></div>
+                <div class="seat occupiedseat"></div>
+                <div class="seat occupiedseat"></div>
+                <div class="seat occupiedseat"></div>
+                <div class="seat occupiedseat"></div>
+                <div class="seat occupiedseat"></div>
+                <div class="seat"></div>
+                <div class="seat occupiedseat"></div>
+            </div>
+
+         <p class="text">You have selected <span id="count">0</span> seats.</p>
+
+    <script src="seating.js"></script>
+   </section>   
+
+</body>
+</html>
+
+<?php
+require_once __DIR__ . "/footer.php";
+?>
